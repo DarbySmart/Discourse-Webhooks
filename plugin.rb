9943c6ab3daece4c2ce718cbb25e96f84738f0f2
Karl Mendes <karl@darbysmart.com>
2016-05-19 21:28:15 -0700

    Fix error

--- plugin.rb
+++ plugin.rb
@@ -8,36 +8,40 @@ after_initialize do
   add_model_callback :notification, :after_create do
     return unless SiteSetting.webhooks_enabled
 
-    params = {}
+    begin
+      params = {}
 
-    if SiteSetting.webhooks_include_api_key
-      api_key = ApiKey.find_by(user_id: nil)
-      if not api_key
-        Rails.logger.warn('Webhooks configured to include the "All User" API key, but it does not exist.')
-      else
-        params[:api_key] = api_key.key
+      if SiteSetting.webhooks_include_api_key
+        api_key = ApiKey.find_by(user_id: nil)
+        if not api_key
+          Rails.logger.warn('Webhooks configured to include the "All User" API key, but it does not exist.')
+        else
+          params[:api_key] = api_key.key
+        end
       end
-    end
 
-    user = self.user
-    params[:user_id] = user.external_id
-    params[:unread_notifications] = user.total_unread_notifications
+      user = self.user
+      params[:user_id] = user.single_sign_on_record.external_id
+      params[:unread_notifications] = user.total_unread_notifications
 
-    uri = URI.parse(SiteSetting.webhooks_url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == 'https'
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless SiteSetting.webhooks_verify_ssl
+      uri = URI.parse(SiteSetting.webhooks_url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE unless SiteSetting.webhooks_verify_ssl
 
-    request = Net::HTTP::Post.new(uri.path)
-    request.add_field('Content-Type', 'application/json')
-    request.body = params.to_json
+      request = Net::HTTP::Post.new(uri.path)
+      request.add_field('Content-Type', 'application/json')
+      request.body = params.to_json
 
-    response = http.request(request)
-    case response
-    when Net::HTTPSuccess then
-      # nothing
-    else
-      Rails.logger.error("#{uri}: #{response.code} - #{response.message}")
+      response = http.request(request)
+      case response
+      when Net::HTTPSuccess then
+        # nothing
+      else
+        Rails.logger.error("#{uri}: #{response.code} - #{response.message}")
+      end
+    rescue Exception => e
+      Rails.logger.error("Notification error: #{e.message}")
     end
   end
 end
